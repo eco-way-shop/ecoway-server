7d5b0f27702e515d99e0b6157bfe115883d562b8
kapatsila-mariia <m.kapatsila@gmail.com>
2024-05-20 19:04:44 +0300

    fix test
    fix tests after sonarcloud

--- spec/requests/authentication_request_spec.rb
+++ spec/requests/authentication_request_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Authentications', type: :request do
   describe 'POST /login' do
     let(:user) { FactoryBot.create(:user, username: 'acushla', password: 'password') }
-
+    let(:login_url) { '/api/v1/login' }
     it 'authenticates the user' do
-      post '/api/v1/login', params: { username: user.username, password: 'password' }
+      post login_url, params: { username: user.username, password: 'password' }
 
       expect(response).to have_http_status(:created)
       expect(json).to eq({
@@ -17,7 +17,7 @@ RSpec.describe 'Authentications', type: :request do
     end
 
     it 'returns error when username does not exist' do
-      post '/api/v1/login', params: { username: 'ac', password: 'password' }
+      post login_url, params: { username: 'ac', password: 'password' }
 
       expect(response).to have_http_status(:unauthorized)
       expect(json).to eq({
@@ -26,11 +26,11 @@ RSpec.describe 'Authentications', type: :request do
     end
 
     it 'returns error when password is incorrect' do
-      post '/api/v1/login', params: { username: user.username, password: 'incorrect' }
+      post login_url, params: { username: user.username, password: 'incorrect' }
 
       expect(response).to have_http_status(:unauthorized)
       expect(json).to eq({
-                           'error' => 'Incorrect password '
+                           'error' => 'Хибний пароль'
                          })
     end
   end
