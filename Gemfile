baa1c2622cd76dd02f128c72440a2dd95af3be25
kapatsila-mariia <m.kapatsila@gmail.com>
2024-05-25 16:26:19 +0300

    fixes

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'rack-cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'byebug'
   gem 'database_cleaner'
   gem 'dotenv-rails'
   gem 'factory_bot_rails'
@@ -49,4 +49,4 @@ group :test do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+gem 'tzinfo-data'
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: :index
       resources :favourites, only: %i[index create destroy]
-      resources :cars, only: %i[index create show destroy]
+      resources :cars, only: %i[index  show destroy]
       # resources :test_drives, only: %i[index destroy]
-
+      post 'add_car', to: 'cars#create'
       post 'test_drive', to: 'test_drives#create'
       post 'login', to: 'authentication#create'
       post 'register', to: 'users#create'
