19f557dd135be77bbc85dc32e98e263e12be7e01
kapatsila-mariia <m.kapatsila@gmail.com>
2024-06-23 15:58:20 +0300

    seed and fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,63 @@
 User.create(username: 'user1', password: 'password')
 User.create(username: 'admin', password: 'password', admin: true)
 
-# 10.times do
-#   admin_user = User.last
-#   make_and_model = Faker::Vehicle.make_and_model
-#   new_car = admin_user.cars.create!(make: make_and_model.split(' ').first, model: make_and_model.split(' ').last,
-#                                     year: Faker::Vehicle.year, ac: 'true', transmission: Faker::Vehicle.transmission,
-#                                     color: Faker::Vehicle.color, max_capacity: 4, price: 10000, desc: "Машина 2019 року", 
-#                                     odometer: 33000, battery: 300,
-#                                     image_url: "https://source.unsplash.com/800x600/?car,#{Faker::Vehicle.color.downcase}")
-#   new_car.save
-# end
+admin_user.cars.create!(
+  make: "Chevy",
+  model: "Bolt EV",
+  year: "2020",
+  ac: 'true',
+  transmission: "Автомат",
+  color: "сірий",
+  max_capacity: 5,
+  price: 32000,
+  desc: "Автомобіль 2020 року випуску з Пробігом в 15000 км З посиленною та оновленою батареєю.",
+  odometer: 15000,
+  battery: 259,
+  image_url: "https://itc.ua/wp-content/uploads/2023/12/2020-chevrolet-bolt-ev-premier-101-1588025513.jpeg"
+)
+
+
+admin_user.cars.create!(
+  make: "Tesla",
+  model: "Model S",
+  year: "2021",
+  ac: 'true',
+  transmission: "Автомат",
+  color:"білий",
+  max_capacity: 5,
+  price: 29000,
+  desc: "Тесла пригнана з Європи, без ДТП Повний привід Пневмопідвіска. Багата комплектація: панорамний дах, підігрів усіх сидінь та руля, камера, електро багажник , автоматичне дальнє світло",
+  odometer: 10000,
+  battery: 405,
+  image_url: "https://teslapark.com.ua/wp-content/uploads/2023/01/IMG_3195-768x512.jpg"
+)
+
+admin_user.cars.create!(
+  make: "Ford",
+  model: "Mustang Mach-E",
+  year: "2021",
+  ac: 'true',
+  transmission: "Автомат",
+  color: "блакитний",
+  max_capacity: 7,
+  price: 34000,
+  desc: "Машина без пошкоджень в багатій комплектації. Без жодного підфарбуваня.",
+  odometer: 20000,
+  battery: 314,
+  image_url: "https://media.cnn.com/api/v1/images/stellar/prod/201214151931-01-ford-mustang-mach-e-electric-suv.jpg?q=w_1110,c_fill/f_webp"
+)
+
+new_car = admin_user.cars.create!(
+  make: "Nissan",
+  model: "Leaf",
+  year: "2014",
+  ac: 'true',
+  transmission: "Автомат",
+  color: "білий",
+  max_capacity: 5,
+  price: 12000,
+  desc: "свіжопригнаний (з Норвегії) розмитнений та поставлений не український облік;- тепловий насос;- максимальна комплектація;- Датчик світла дощу;- Камера 360;- шкіряний салон;- підігрів керма;- підігрів передніх та задніх сидінь;- безключовий доступ;- автомобільне складання дзеркал;- сонячна батарея;- Кнопка старт стоп;- Led оптика;- преміум музика BOSE;- Bluetooth, aux, hands-free;- Навігація;- 12 airbag;- сервісна книга, два ключі запалювання;- обслугована не потребує вкладень;- є всі перевірки роботи і життя батареї",
+  odometer: 40000,
+  battery: 150,
+  image_url: "https://img.automoto.ua/auto/Nissan-Leaf-belyiy-Hetchbek-2014-fb7-63653576.webp"
+
