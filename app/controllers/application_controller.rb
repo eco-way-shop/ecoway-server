7dfa7969c3ba1cd68f78e7c1b498428226474548
kapatsila-mariia <m.kapatsila@gmail.com>
2024-06-23 17:44:11 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,12 @@ class ApplicationController < ActionController::API
     non_admin_authentication unless @current_user.admin == true
   end
 
-  # def authenticate_request!
-  #   return invalid_authentication if !payload || !AuthenticationTokenService.valid_payload(payload.first)
+  def authenticate_request!
+    return invalid_authentication if !payload || !AuthenticationTokenService.valid_payload(payload.first)
 
-  #   current_user!
-  #   invalid_authentication unless @current_user
-  # end
+    current_user!
+    invalid_authentication unless @current_user
+  end
 
   def current_user!
     @current_user = User.find_by(id: payload[0]['user_id'])
